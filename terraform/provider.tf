cc17f13acf2435974be3097140ce5c123b0d6fa9
Felistus Kimanthi <kimanthi.felistus@gmail.com>
2026-05-16 13:19:13 +0300

    changes in terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket         = "kk-terraform-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "kk-tf-lock"
-    encrypt        = true
+  #   bucket         = "kijanikiosk-terraform-state"
+  #   key            = "global/s3/terraform.tfstate"
+  #   region         = "us-west-2"
+  #   dynamodb_table = "kk-tf-lock"
+  #   encrypt        = true
+  # }
   }
 }
 
